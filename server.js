2d24d2508df607fa9bbbfd04f421b70049251a2b
dima <macuser@macusers-iMac-4.local>
2016-05-08 22:03:41 +1000

    Updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;  //get system PORT
 
 
 var middleware = require('./middleware');
@@ -13,12 +13,6 @@ app.get('/about', middleware.requireAuth, function(req, res) {
 
 app.use(express.static(__dirname + '/public'));  // will use default index.thml in 'public' folder
 
-// app.listen(PORT, function () {
-//     console.log('Express server started on port ' + PORT );
-// });
-
-
-var buf = new Buffer ("hello", "utf8");
-
-console.log(buf);
-console.log(buf.toString());
+app.listen(PORT, function () {
+    console.log('Express server started on port ' + PORT );
+});
